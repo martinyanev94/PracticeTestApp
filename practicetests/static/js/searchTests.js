4ae232aea44055b716d3e72f28033432c2674a5f
martinyanev94 <martinyanev94@gmail.com>
2023-08-08 17:53:25 -0400

    Add buttons to the My Tests table when updated with the js

--- practicetests/static/js/searchTests.js
+++ practicetests/static/js/searchTests.js
@@ -1,14 +1,14 @@
-const searchField = document.querySelector("#searchField");
+        const searchField = document.querySelector("#searchField");
 const tableOutput = document.querySelector(".table-output");
 const appTable = document.querySelector(".app-table");
-const paginationContainer = document.querySelector(".pagination-container");
 tableOutput.style.display = "none";
+const paginationContainer = document.querySelector(".pagination-container");
 const noResults = document.querySelector(".no-results");
 const tbody = document.querySelector(".table-body");
+
+
 searchField.addEventListener("keyup", (e) => {
   const searchValue = e.target.value;
-
-
 
   if (searchValue.trim().length > 0) {
     paginationContainer.style.display = "none";
@@ -31,29 +31,23 @@ searchField.addEventListener("keyup", (e) => {
           noResults.style.display = "none";
           data.forEach((item) => {
             tbody.innerHTML += `
-                <tr>
+              <tr>
                 <td>${item.header}</td>
                 <td>${item.notes}</td>
                 <td>${item.num_questions}</td>
                 <td>${item.created_at}</td>
-        <td>
-                    <a
-            href="{% url 'edit-test' user_tests.id  %}"
-            class="btn btn-secondary btn-sm"
-            >Edit</a
-          >
-
-        </td>
-          <td>
-                    <a
-            href="{% url 'delete-test' user_tests.id  %}"
-            class="btn btn-danger btn-sm"
-            >Delete</a
-          >
-        </td>
-      </tr>
-          `;
+                <td>
+                  <a href="#" class="btn btn-secondary btn-sm edit-button" data-id="${item.id}">Edit</a>
+                </td>
+                <td>
+                  <a href="#" class="btn btn-danger btn-sm delete-button" data-id="${item.id}">Delete</a>
+                </td>
+              </tr>
+            `;
           });
+
+          // Attach event listeners to dynamically created buttons
+          attachButtonEventListeners();
         }
       });
   } else {
@@ -62,3 +56,50 @@ searchField.addEventListener("keyup", (e) => {
     paginationContainer.style.display = "block";
   }
 });
+
+function attachButtonEventListeners() {
+  const editButtons = document.querySelectorAll(".edit-button");
+  const deleteButtons = document.querySelectorAll(".delete-button");
+   const downloadStudentViewButtons = document.querySelectorAll(".download-student-view");
+
+  editButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = e.target.getAttribute("data-id");
+      const editUrl = `/my-tests/edit-test/${id}`;
+      window.location.href = editUrl;
+    });
+  });
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = e.target.getAttribute("data-id");
+      if (confirmDelete()) {
+        const deleteUrl = `/my-tests/delete-test/${id}`;
+        window.location.href = deleteUrl;
+      }
+    });
+  });
+    deleteButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = e.target.getAttribute("data-id");
+      if (confirmDelete()) {
+        const deleteUrl = `/my-tests/delete-test/${id}`;
+        window.location.href = deleteUrl;
+      }
+    });
+  });
+      downloadStudentViewButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      const id = e.target.getAttribute("data-id");
+      if (confirmDelete()) {
+        const downloadStudentViewUrl = `/my-tests/download-student-view/${id}`;
+        window.location.href = deleteUrl;
+      }
+    });
+  });
+
+}
