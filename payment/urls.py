bc4d0284b5f42ac0d0ab557ac939b5b55fad781a
martinyanev94 <martinyanev94@gmail.com>
2023-08-25 17:45:34 -0400

    Add plans html

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import MembershipSelectView, PaymentView, updateTransactionRecords, profile_view, cancelSubscription
 
 urlpatterns = [
     path('', views.payment_plans, name='payment-plans'),
+    path('', MembershipSelectView.as_view(), name='select'),
+    path('payment/', PaymentView, name='payment'),
+    path('update-transactions/<subscription_id>/',
+         updateTransactionRecords, name='update-transactions'),
+    path('profile/', profile_view, name='profile'),
+    path('cancel/', cancelSubscription, name='cancel')
 
 ]
