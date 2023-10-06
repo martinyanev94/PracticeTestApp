2b6ece50d1f6b7795ed0aa6ac52cdf901d8ddb4e
martinyanev <martinyanev94@gmail.com>
2023-10-06 18:19:13 -0400

    Update hostnames

--- practicetests/settings.py
+++ practicetests/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], "app.testboxai.com"] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'],  "app.testboxai.com"] if 'WEBSITE_HOSTNAME' in os.environ else [ "app.testboxai.com"]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 
